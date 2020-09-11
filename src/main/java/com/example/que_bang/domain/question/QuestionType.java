17e01da7974d20a035edab8be62ac813842a11bd
robin <mgh3326@naver.com>
2020-09-11 19:11:43 +0900

    add QuestionType

--- src/main/java/com/example/que_bang/domain/question/QuestionType.java
+++ src/main/java/com/example/que_bang/domain/question/QuestionType.java
@@ -1,2 +1,12 @@
-package com.example.que_bang.domain.question;public class QuestionType {
+package com.example.que_bang.domain.question;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum QuestionType {
+  ESSAY, MULTIPLE_CHOICE, SHORT_ANSWER;
+
+  public String getDiscriminatorValue() {
+    return name().substring(0, 1);
+  }
 }
