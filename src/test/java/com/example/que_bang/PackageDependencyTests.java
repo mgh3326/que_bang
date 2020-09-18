80d187667a85d53671f3cfeec6f391c818ecf163
robin <mgh3326@naver.com>
2020-09-18 19:38:43 +0900

    ArchTest 추가

--- src/test/java/com/example/que_bang/PackageDependencyTests.java
+++ src/test/java/com/example/que_bang/PackageDependencyTests.java
@@ -1,4 +1,25 @@
 package com.example.que_bang;
 
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packagesOf = App.class)
 public class PackageDependencyTests {
+  private static final String ACOUNT = "..modules.account..";
+  private static final String QUESTION = "..modules.question..";
+  private static final String QUESTION_BUNDLE = "..modules.question_bundle..";
+  private static final String TEST_PAPER_QUESTION_BUNDLE = "..modules.test_paper_question_bundle..";
+  private static final String TEST_PAPER = "..modules.test_paper..";
+
+  @ArchTest
+  ArchRule modulesPackageRule = classes().that().resideInAPackage("com.example.que_bang.modules..")
+          .should().onlyBeAccessed().byClassesThat()
+          .resideInAnyPackage("com.example.que_bang.modules..");
+  @ArchTest
+  ArchRule studyPackageRule = classes().that().resideInAPackage(QUESTION_BUNDLE)
+          .should().onlyBeAccessed().byClassesThat()
+          .resideInAnyPackage(QUESTION_BUNDLE, QUESTION, TEST_PAPER_QUESTION_BUNDLE, TEST_PAPER);
 }
